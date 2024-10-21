ce4e3e6673fe862ac70af13409375067fee59bb1
NezzDevs <nezzisgod@gmail.com>
2024-10-20 23:37:49 -0400

    Update torrent_galaxy.py

--- torrents/torrent_galaxy.py
+++ torrents/torrent_galaxy.py
@@ -1,10 +1,8 @@
-import re
 import time
 import aiohttp
 from bs4 import BeautifulSoup
 from helper.html_scraper import Scraper
 from constants.base_url import TGX
-
 
 class TorrentGalaxy:
     def __init__(self):
@@ -17,64 +15,19 @@ class TorrentGalaxy:
             my_dict = {"data": []}
             root_div = soup.find("div", class_="gluewrapper")
             post_nd_torrents = root_div.find_next("div").find_all("div")
-            poster = post_nd_torrents[1].find("img")["data-src"]
             torrentsand_all = post_nd_torrents[4].find_all("a")
-            torrent_link = torrentsand_all[0]["href"]
             magnet_link = torrentsand_all[1]["href"]
-            direct_link = self.BASE_URL + torrentsand_all[2]["href"]
 
             details_root = soup.find("div", class_="gluewrapper").select(
                 "div > :nth-child(2) > div > .tprow"
             )
 
             name = details_root[0].find_all("div")[-1].get_text(strip=True)
-            category = (
-                details_root[3].find_all("div")[-1].get_text(strip=True).split(">")[0]
-            )
-            languagee = details_root[4].find_all("div")[-1].get_text(strip=True)
-            size = details_root[5].find_all("div")[-1].get_text(strip=True)
-            hash = details_root[6].find_all("div")[-1].get_text(strip=True)
-            username = (
-                details_root[7]
-                .find_all("div")[-1]
-                .find("span", class_="username")
-                .get_text(strip=True)
-            )
-            date_up = details_root[8].find_all("div")[-1].get_text(strip=True)
 
-            btns = details_root[10].find_all("button")
-            seeders = btns[0].find("span").get_text(strip=True)
-            leechers = btns[1].find("span").get_text(strip=True)
-            downloads = btns[2].find("span").get_text(strip=True)
-            imdb_id = soup.select_one("#imdbpage")["href"].split("/")[-1]
-            genre_list = [
-                x.get_text(strip=True) for x in details_root[11].find_all("a")
-            ]
-            soup.find("div", id="intblockslide").find_all("a")
-            imgs = [
-                img["href"]
-                for img in (soup.find("div", id="intblockslide").find_all("a"))
-                if img["href"].endswith((".png", ".jpg", ".jpeg"))
-            ]
             my_dict["data"].append(
                 {
                     "name": name,
-                    "size": size,
-                    "seeders": seeders,
-                    "language": languagee,
-                    "leechers": leechers,
-                    "category": category,
-                    "uploader": username,
-                    "downloads": downloads,
-                    "poster": poster,
-                    "direct_download_link": direct_link,
-                    "imdb_id": imdb_id,
-                    "hash": hash,
                     "magnet": magnet_link,
-                    "torrent": torrent_link,
-                    "screenshot": imgs,
-                    "genre": genre_list,
-                    "date": date_up,
                 }
             )
             return my_dict
@@ -91,63 +44,19 @@ class TorrentGalaxy:
                     div = divs.find_all("div")
                     try:
                         name = div[4].find("a").get_text(strip=True)
-                        imdb_url = (div[4].find_all("a"))[-1]["href"]
                     except:
                         name = (div[1].find("a", class_="txlight")).find("b").text
-                        imdb_url = (div[1].find_all("a"))[-1]["href"]
 
                     if name != "":
                         try:
                             magnet = div[5].find_all("a")[1]["href"]
-                            torrent = div[5].find_all("a")[0]["href"]
                         except:
                             magnet = div[3].find_all("a")[1]["href"]
-                            torrent = div[3].find_all("a")[0]["href"]
-                        size = soup.select("span.badge.badge-secondary.txlight")[
-                            idx
-                        ].text
-                        try:
-                            url = div[4].find("a")["href"]
-                        except:
-                            url = div[1].find("a", class_="txlight")["href"]
-                        try:
-                            date = div[12].get_text(strip=True)
-                        except:
-                            date = div[10].get_text(strip=True)
-                        try:
-                            seeders_leechers = div[11].find_all("b")
-                            seeders = seeders_leechers[0].text
-                            leechers = seeders_leechers[1].text
-                        except:
-                            seeders_leechers = div[11].find_all("b")
-                            seeders = seeders_leechers[0].text
-                            leechers = seeders_leechers[1].text
-                        try:
-                            uploader = (div[7].find("a")).find("span").text
-                        except:
-                            uploader = (div[5].find("a")).find("span").text
-                        try:
-                            category = (
-                                div[0].find("small").text.replace("&nbsp", "")
-                            ).split(":")[0]
-                        except:
-                            category = None
+
                         my_dict["data"].append(
                             {
                                 "name": name,
-                                "size": size,
-                                "seeders": seeders,
-                                "leechers": leechers,
-                                "category": category,
-                                "uploader": uploader,
-                                "imdb_id": imdb_url.split("=")[-1],
-                                "hash": re.search(
-                                    r"([{a-f\d,A-F\d}]{32,40})\b", magnet
-                                ).group(0),
                                 "magnet": magnet,
-                                "torrent": torrent,
-                                "url": self.BASE_URL + url,
-                                "date": date,
                             }
                         )
                     if len(my_dict["data"]) == self.LIMIT:
@@ -164,7 +73,6 @@ class TorrentGalaxy:
                 except:
                     my_dict["current_page"] = None
                     my_dict["total_pages"] = None
-                    # ...
                 return my_dict
         except:
             return None
@@ -223,5 +131,3 @@ class TorrentGalaxy:
                     )
                 )
             return await self.parser_result(start_time, url, session)
-
-    #! Maybe Implemented in Future
